bfd31d62bb9e89abd047e65c16779a1d2e89b874
Kayla Comalli <Kayla.comalli@gmail.com>
2018-06-26 18:52:41 -0400

    closer, still not fully figured out

---
+++ README.md
@@ -0,0 +1,42 @@
+## Permutation process (recursive)
+
+* p = chars to permute
+* n = number of chars in string
+* r = permutation char size
+* result = final result
+* pos = index position for result
+* finalArr = array of array of the char permutations
+
+Input is string of chars, let's say `'abc'`
+
+1. establish base case: `(n == 0) return;`  
+2. Set up `for` loop based on length of `n`
+3. inside `for` loop, create a copy var `char` p -- `pCopy` (`abc`)
+4. until pCopy is null, pop off first letter from `pCopy` ('a') and store it as the first index of the results array
+5. when result is complete, push it into the array
+6. 
+
+
+example: 
+p = 'abc'
+n = 3
+r = 2
+result = char[3]
+pos = 0
+arr = poinst * char
+
+// first pass
+void permute(p, n, r, result, pos, arr) {
+	char pCopy = p;
+	if (result.length == r) {
+		arr << result;
+		pos = 0;
+		return;
+	}	
+	for (int i =0; i < n; i++) {
+		result[i] = p.pop();
+		permute(p, n--, r--, result, pos++, arr);
+	}
+}
+
+
--- permute.cpp
+++ permute.cpp
@@ -3,22 +3,43 @@
 
 using namespace std;
 
-//void permute(P, n, r, result. pos) {
-// string "abcd"
-//
-//}
+
+// p = string to permute
+// n = number of chars
+// r = permutation size
+// result = final result
+// pos = index position for result (might not be necessary)
+
+void permute(char p[], int n, int r, char result[], int pos) {
+  if (n == 0) {
+    cout << "result: " << result << endl;
+    pos = 0;
+    return;
+  }
+  for (int i=0; i < n; i++) {
+    char pNew[n];
+    strncpy(pNew, p, n);
+    result[pos] = pNew[pos];
+    pNew[0] = pNew[1];
+    pNew[n] = '\0';
+    permute(pNew, n-1, r-1, result, pos+1);
+  }
+}
 
 int main()
 {
   cout << "Number of chars (n): ";
   unsigned int n;
   cin >> n;
+  cout << n << endl;
   char* alphabets = new char [n+1];
   cout << "Alphabets: ";
   cin >> alphabets;
+  cout << alphabets << endl;
   cout << "Number of alphabets in the permutations (r): ";
   unsigned int r;
   cin >> r;
+  cout << r << endl;
   if (r > n) {
     cout << "r cannot be greater than n" << endl;
     exit(1);
@@ -27,11 +48,11 @@ int main()
 	<< alphabets 
 	<< " taking " 
 	<< r 
-	<< " alphabets at a time." 
+	<< " alphabets at a time."
 	<< endl;
+  char result[3];
   // Print the permutations of n alphabets taken r at a time
-
-  
-
+  permute(alphabets, n, r, result, 0);
+  delete[] alphabets;
   return 0;
 }
